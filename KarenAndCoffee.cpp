3af001141dee54201d42713c6c00e84948bcd84f
OsmarGHz <osmarjavier1@gmail.com>
2024-03-07 21:19:19 -0600

    Added 1st version of KarenAndCoffee's Problem

---
+++ KarenAndCoffee.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i,n,k,q;
+    scanf("%d %d %d",&n,&k,&q);
+    int recetas[n][2],queries[q][2];
+    for (i = 0; i < n; i++){
+        scanf("%d %d",&recetas[i][0],&recetas[i][1]);
+    }
+    for (i = 0; i < q; i++){
+        scanf("%d %d",&queries[i][0],&queries[i][1]);
+    }
+    
+}
