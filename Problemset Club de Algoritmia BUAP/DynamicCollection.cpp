8a4051258e0d4c544aa5aa138ff2d43f05b82708
OsmarGHz <osmarjavier1@gmail.com>
2024-08-05 23:11:12 -0600

    Updated DinamicCollection.cpp file, still in development

--- Problemset Club de Algoritmia BUAP/DynamicCollection.cpp
+++ Problemset Club de Algoritmia BUAP/DynamicCollection.cpp
@@ -5,52 +5,62 @@
 using namespace std;
 typedef long long lli;
 
+int closestToTheRight(int param1, vector<int> numbers, int n){
+    int left=0, right=n-1, mid, res=-1;
+    while (left<=right){
+        mid = (left + right)/2;
+        if (param1 > numbers[mid]){
+            left = mid+1;
+        }else{
+            res = mid;
+            right = mid-1;
+        }
+    }
+    return res;
+}
+
+int closestToTheLeft(int param1, vector<int> numbers, int n){
+    int left=0, right=n-1, mid, res=n;
+    while (left<=right){
+        mid = (left + right)/2;
+        if (param1 >= numbers[mid]){
+            res = mid;
+            left = mid+1;
+        }else{
+            right = mid-1;
+        }
+    }
+    return res;
+}
+
 int main(){
     cin.tie(0);
     ios_base::sync_with_stdio(false);
-    int n,q,value; cin >> n >> q;
-    map <int,int> collection;
-    vector <int> numberOfNumbers(1000000000,0);
-    vector <lli> slideNumbers(1000000000,0);
+    int n,q; cin >> n >> q;
+    vector <int> numbers(n);
     for (int i = 0; i < n; i++){
-        cin >> value;
-        if (collection.count(value)){
-            collection[value]++;
-        }else{
-            //collection.insert({value,1});
-            collection.insert(pair<int,int> (value,1));
-        }
+        cin >> numbers[i];
     }
-    int election, param1, param2, counter=0;
-    map <int,int>::iterator it;
+    sort(numbers.begin(), numbers.end());
+    int election[q], param1[q], param2[q], counter=0, position1, position2, pos;
     for (int i = 0; i < q; i++){
-        cin >> election;
-        if (election==1){
-            cin >> param1;
-            if (collection.count(param1)) continue;
-            if (param1 >= (*--collection.end()).first){ // Param1 vs greatest element
-                collection.insert(pair<int,int> (param1,1));
+        cin >> election[i] >> param1[i];
+        if(election[i]==2) cin >> param2[i];
+    }
+
+    for (int i = 0; i < q; i++){
+        if (election[i]==1){
+            if (param1[i] > numbers[n-1]){ // Param1 vs greatest element
+                numbers.push_back(param1[i]);
+                n++;
                 continue;
-            }else{
-                collection.insert(pair<int,int> (param1,1));
-                it = ++collection.find(param1);
-                if (it->second > 1){
-                    it->second--;
-                }else{
-                    collection.erase(it);
-                }
             }
+            pos = closestToTheRight(param1[i], numbers, n);
+            if (numbers[pos]!=param1[i]) numbers[pos] = param1[i];
         }else{
-            cin >> param1 >> param2;
-            counter = 0;
-            it = collection.find(param1);
-            if (it==collection.end()){
-                it = collection.upper_bound(param1);
-            }
-            while (it != collection.end() && it->first<=param2){
-                counter+=it->second;
-                it++;
-            }
+            position1 = closestToTheRight(param1[i], numbers, n);
+            position2 = closestToTheLeft(param2[i], numbers, n);
+            counter = position2 - position1 + 1;
             cout << counter << "\n";
         }
     }
