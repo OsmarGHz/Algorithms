50138898a19eff9e9a0ac5d45852ba153bdbfc87
OsmarGHz <osmarjavier1@gmail.com>
2024-11-18 14:00:16 -0600

    Added findingPrivacy.cpp

---
+++ FindingPrivacy.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int k,n,i;
+    cin >> k >> n;
+    string a(3*k,'-');
+    if (3*k<n || (2*k)-1>n) a = "*";
+    else{
+        i=1;
+        while (i<3*k){
+            a[i]='X';
+            i+=3;
+        }
+        if (3*k!=n){
+            i=0;
+            while (i<a.length() && a.length()!=n){
+                a.erase(a.begin()+i);
+                i+=2;
+            }
+            if (a.length()!=n) a.pop_back();
+        }
+    }
+    cout << a;
+}
