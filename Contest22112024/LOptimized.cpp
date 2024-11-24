e60592cf9929b6ba6de9b1eee59a1dc6569a551e
OsmarGHz <osmarjavier1@gmail.com>
2024-11-24 13:53:16 -0600

    Backup 24-11-2024

---
+++ Contest22112024/LOptimized.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using fore = for(int )
+using namespace std;
+using lli = long long int;
+
+int main(){
+    int n,i,j;
+    lli res;
+    cin >> n;
+    vector <lli> a(n), nBitsA(n), nBitsGlobal(32,0);
+    for (i = 0; i < n; i++) cin >> a[i];
+    for (i = 0; i < n; i++){
+        a[i];
+        for (j = 0; temp!=0 ; j++){
+            if (temp%2==1) nBitsGlobal[j]++;
+            temp = temp >> 1;
+        }
+        nBitsA[i] = j;
+    }
+    sort(nBitsA.begin(),nBitsA.end());
+    reverse(nBitsA.begin(),nBitsA.end());
+    for (i = 0; i < n; i++){
+        res = 0;
+        if (nBitsGlobal[nBitsA[i]-1]>=1){
+            res += 1;
+            nBitsGlobal[nBitsA[i]-1]--;
+        }
+        for (j = nBitsA[i]-2; j >= 0; j--){
+            res = res << 1;
+            if (nBitsGlobal[j]>=1){
+                res += 1;
+                nBitsGlobal[j]--;
+            }
+        }
+        cout << res << " ";
+    }
+}
