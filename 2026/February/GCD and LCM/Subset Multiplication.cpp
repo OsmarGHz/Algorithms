d06506f5f35c9e8788843f11c598e9225e771cfd
OsmarGHz <osmarjavier1@gmail.com>
2026-03-16 00:41:01 -0600

    Backup 15-3-2026

--- 2026/February/GCD and LCM/Subset Multiplication.cpp
+++ 2026/February/GCD and LCM/Subset Multiplication.cpp
@@ -3,13 +3,21 @@ using namespace std;
 using ll = long long;
 
 void solve(){
-    int n;
+    int n,gcdActual,lcmActual;
     cin>>n;
     vector<int> b(n);
     for (int i = 0; i < n; i++){
         cin>>b[i];
     }
+
+    gcdActual = gcd(b[0],b[1]);
+    lcmActual = b[0]/gcdActual;
+    for (int i = 1; i < n-1; i++){
+        gcdActual = gcd(b[i],b[i+1]);
+        lcmActual = lcm(lcmActual,b[i]/gcdActual);
+    }
     
+    cout << lcmActual;
 }
 
 int main(){
