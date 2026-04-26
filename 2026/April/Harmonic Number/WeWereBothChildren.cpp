6968d25b1eea9752c3ed8e9f2cf0705580375a44
OsmarGHz <osmarjavier1@gmail.com>
2026-04-26 00:23:29 -0600

    Added we were both children template

---
+++ 2026/April/Harmonic Number/WeWereBothChildren.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+using ll = long long;
+
+void solve(){
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+        cout << "\n";
+    }
+}
