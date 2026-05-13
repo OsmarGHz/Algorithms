89738d89edfd2734b3b11bf571b5d43382d0c73f
OsmarGHz <osmarjavier1@gmail.com>
2026-05-13 00:49:50 -0600

    Added COMDIV

--- 2026/April/Harmonic Number/WeWereBothChildren.cpp
+++ 2026/April/Harmonic Number/WeWereBothChildren.cpp
@@ -4,23 +4,34 @@ using namespace std;
 using ll = long long;
 
 void solve(){
-    int n;
+    int n,maxi=0;
     cin>>n;
-    vector<int> a(n);
+    vector<int> a(n), frecuencias(n+1,0);
     for (int i = 0; i < n; i++){
         cin>>a[i];
     }
-    vector<int> b(n+1,0);
-    for(int i=0;i<n;i++){
-        for(int j=a[i];j<=n;j+=a[i]){
-            b[j]++;
+    for (int i = 0; i < n; i++){
+        if(a[i]<=n){
+            frecuencias[a[i]]++;
         }
     }
-    sort(b.begin(),b.end());
-    cout << b[n];
+    vector<int> b(n+1,0);
+    for(int i=1;i<=n;i++){
+        if(frecuencias[i]!=0){
+            for(int j=i;j<=n;j+=i){
+                b[j]+=frecuencias[i];
+                if(b[j]>maxi){
+                    maxi=b[j];
+                }
+            }
+        }
+    }
+    cout << maxi;
 }
 
 int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
     int t;
     cin>>t;
     while(t--){
